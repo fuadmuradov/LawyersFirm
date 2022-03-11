48a98d1a93f89b44dd695633ebc2d9ae1ef2f68c
fuadmuradov <1fuad.muradov@gmail.com>
2022-03-11 23:55:59 +0400

    Add project files.

---
+++ LawyersFirm/Controllers/AboutController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LawyersFirm.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+
+        public IActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
---
+++ LawyersFirm/Controllers/PracticeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LawyersFirm.Controllers
+{
+    public class PracticeController : Controller
+    {
+        public IActionResult AllPractices()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
